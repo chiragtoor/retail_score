8da6f4a4cfc9f698a510fbb85d12597acc47956d
Chirag Singh Toor <chirag@Chirags-MacBook-Pro.local>
2016-08-02 09:45:19 -0700

    Custom mix task for building assets

---
+++ lib/mix/task/digest.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.RetailScore.Digest do
+  use Mix.Task
+
+  def run(args) do
+    Mix.Shell.IO.cmd "NODE_ENV=production ./node_modules/webpack/bin/webpack.js -p"
+    Mix.Shell.IO.cmd "NODE_ENV=production webpack ./webpack.server.config.js"
+    :ok = Mix.Tasks.Phoenix.Digest.run(args)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -50,6 +50,7 @@ defmodule RetailScore.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "ecto.migrate", "test"],
+     "phoenix.digest": ["retail_score.digest"]]
   end
 end
