afe19a6441a991cf25c77843538a104f867083c8
Chirag Singh Toor <chirag@Chirags-MacBook-Pro.local>
2016-08-02 13:06:08 -0700

    Stop SSR, see if that is the issue

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,17 +2,19 @@ defmodule RetailScore.PageController do
   use RetailScore.Web, :controller
 
   def index(conn, _params) do
-    initial_state = %{"visitors" => %{"max_online" => 25, "total" => 15, "online" => 15}}
-    props = %{
-      "location" => conn.request_path,
-      "initial_state" => initial_state
-    }
+    # initial_state = %{"visitors" => %{"max_online" => 25, "total" => 15, "online" => 15}}
+    # props = %{
+    #   "location" => conn.request_path,
+    #   "initial_state" => initial_state
+    # }
 
-    result = RetailScore.ReactIO.json_call!(%{
-      component: "./priv/static/js/reph.js",
-      props: props,
-    })
+    # result = RetailScore.ReactIO.json_call!(%{
+    #   component: "./priv/static/js/reph.js",
+    #   props: props,
+    # })
 
-    render(conn, "index.html", html: result["html"], props: initial_state)
+    # render(conn, "index.html", html: result["html"], props: initial_state)
+
+    render(conn, "index.html", html: [], props: %{})
   end
 end
