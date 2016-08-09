978b15f6392a1c4a76d0585b8f1f8456f319c615
Chirag Singh Toor <chirag@Chirags-MacBook-Pro.local>
2016-08-09 14:44:35 -0700

    Try locking to specific version of phoenix_ecto

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule RetailScore.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.0"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
+     {:phoenix_ecto, "~> 3.0.1"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
