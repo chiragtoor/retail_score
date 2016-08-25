c421d5bef32a0ade501b728f5980874e97a8b010
Chirag Singh Toor <chirag@Chirags-MacBook-Pro.local>
2016-08-25 09:57:44 -0700

    remove force_ssl, could be causing url issue

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :retail_score, RetailScore.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "glacial-bayou-31255.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "glacial-bayou-31255.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
