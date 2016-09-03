cfbe02cf8d6614ac4667bf0e0c3fdae302273fae
Chirag Singh Toor <chirag@Chirags-MacBook-Pro.local>
2016-09-03 00:22:21 -0700

    Comments to explain core SRP logic

--- web/static/js/containers/Test.js
+++ web/static/js/containers/Test.js
@@ -5,7 +5,7 @@ import { Row, Col, InputGroup, FormGroup, ButtonGroup, Button, FormControl, Drop
 
 import GoogleMap from '../components/GoogleMap';
     
-const properties = [1, 2, 3, 4];
+const properties = [5, 4, 3, 2, 1];
 
 export class Test extends React.Component {
   constructor(props) {
@@ -13,10 +13,12 @@ export class Test extends React.Component {
 
     this.listingsDivScrolled = this.listingsDivScrolled.bind(this);
 
+    // used on mobile to store all propertyTile refs
     this._propertyTiles = new Map();
 
     this.state = {
       mounted: false,
+      // used to highlight the correct tile on mobile and desktop, will be used to pass into the map which pin to enlarge and center on
       mobileSelectedIndex: 0
     }
   }
@@ -25,18 +27,28 @@ export class Test extends React.Component {
     this.setState({mounted: true});
   }
 
+  // method is called on mobile when the listingsDiv (which contains all tiles horizontally) is scrolled
   listingsDivScrolled() {
+    // value to compare the left edge of all propertyTile's distance to, whichever tile is closest to this value (measured to the right of the left edge)
+    //  is what is considered the main scroll psoition tile
+    const edgeLimit = 100;
+    // method loops through all current mobile property tiles on screen and gets the one in the main scroll position
+    // filter out possible null values
     const minTuple = Array.from(this._propertyTiles.values()).filter(propertyTile => propertyTile != null)
+      // map over and get the actual dom elements left edge using ReactDOM
       .map((propertyTile) => {
         return ReactDOM.findDOMNode(propertyTile).getBoundingClientRect().left;
+      // reduce the array to find which tile's left edge is closest to the main scroll position
       }).reduce((minToLimit, leftEdge, index) => {
-        if(Math.abs(leftEdge - 100) < Math.abs(minToLimit.value - 125)) {
+        if(Math.abs(leftEdge - edgeLimit) < Math.abs(minToLimit.value - edgeLimit)) {
           return {index: index, value: leftEdge};
         } else {
           return minToLimit;
         }
+      // initial values are a null index and very large distance that any tile will be considered closer to
       }, {index: -1, value: 10000});
 
+    // set the state to the new closest tile's index, this way the right tile is highlighted on the device
     this.setState({mobileSelectedIndex: minTuple.index});
   }
 
@@ -83,28 +95,41 @@ export class Test extends React.Component {
             }
             </ReactCSSTransitionGroup>
           </div>
+          {/* Listings section */}
           <div className="col-xs-12 col-md-6 col-md-pull-6 srpContentHeightOther">
             <ReactCSSTransitionGroup transitionName="fadeInUp" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
             {this.state.mounted ?
               <div style={{width:"100%", height:"100%", maxHeight:"100%"}}>
+                {/* Search Bar is above at top on mobile, so hide in that case */}
                 <div className="hidden-sm hidden-xs">
                   <SearchBar />
                 </div>
+                {/* Filters are opened by a button on mobile, so hide in that case */}
                 <div className="hidden-sm hidden-xs" style={{marginBottom:"10px"}}>
                   <Filters />
                 </div>
+                {/* Button for CP edit, # of Listings, and if on mobile button for Filters */}
                 <div style={{borderTop:"solid thin #CCCCCC", borderBottom:"solid thin #CCCCCC", height:"40px", display:"flex", alignItems:"center"}}>
-                  <Button style={{marginTop:"5px", marginBottom:"5px", marginLeft:"15px", float:"right", border:"none",fontSize:"16px", fontWeight:"400"}}><i className="fa fa-user" style={{color:"#49A3DC"}}/></Button>
+                  <Button style={{marginTop:"5px", marginBottom:"5px", marginLeft:"15px", border:"none",fontSize:"16px", fontWeight:"400"}}><i className="fa fa-user" style={{color:"#49A3DC"}}/></Button>
                   <label className="control-label" style={{fontSize:"16px", flexGrow:"1", textAlign:"center"}}>25 Spaces for Lease</label>
-                  <Button className="hidden-md hidden-lg" style={{marginTop:"5px", marginBottom:"5px", marginRight:"15px", float:"right", border:"none",fontSize:"16px", fontWeight:"400"}}><i className="fa fa-filter" style={{color:"#49A3DC"}}/></Button>
+                  <Button className="hidden-md hidden-lg" style={{marginTop:"5px", marginBottom:"5px", marginRight:"15px", border:"none",fontSize:"16px", fontWeight:"400"}}><i className="fa fa-filter" style={{color:"#49A3DC"}}/></Button>
                 </div>
+                {/* 
+                    listingsDiv is what controls the horizontal v vertical scrolling, by default it is mobile first and uses flex-box to go in a row with 
+                      flex-shrink 0 on children so they take up desired width, on mobile width is a set value
+                    on desktops it is still in a row direction, but ads a wrap to it with a set width on listingsDiv, this way content automatically wraps
+                      in order with tile specific sizing CSS setting it to 50% of available space on desktop listings, so there are pairs of 2 going down
+                  */}
+                {/* Rendered twice and hidden, this is to pass in the needed functionality and a flag so that the component does only what is needed only */}
+                {/* Bootstrap hidden is using display:none, so performance of doing this should not be a issue. But even so with pagination we should be avoiding any perfomance problem with many tiles */}
                 <div className="listingsDiv hidden-sm hidden-xs">
                   {properties.map((property, index) => {
-                    return <PropertyTile mobile={false} key={index} index={index} onHover={(index) => this.setState({mobileSelectedIndex: index})} selected={this.state.mobileSelectedIndex == index} style={{flexShrink: "1"}} ref={c => this._propertyTiles.set(index, c)}/>
+                    return <PropertyTile mobile={false} key={index} index={index} onHover={(index) => this.setState({mobileSelectedIndex: index})} selected={this.state.mobileSelectedIndex == index} style={{flexShrink: "1"}}/>
                   })}
                 </div>
                 <div className="listingsDiv hidden-md hidden-lg" onScroll={this.listingsDivScrolled}>
                   {properties.map((property, index) => {
+                    {/* Add the ref of this to the mobile tiles ref storage, this is used for calculating which element is in the main scroll position */}
                     return <PropertyTile mobile={true} key={index} index={index} selected={this.state.mobileSelectedIndex == index} style={{flexShrink: "1"}} ref={c => this._propertyTiles.set(index, c)}/>
                   })}
                 </div>
@@ -121,20 +146,38 @@ export class Test extends React.Component {
 }
 class PropertyTile extends React.Component {
   render() {
-    const selectedPanelContainerStyle = {display:"flex", flexDirection:"column", borderColor:"#49A3DC", borderWidth:"2px", boxShadow:"0 0 2px gray", marginBottom:"0px"};
+  {/* styles and color to use when a tile is currently selected, this is hovered over on desktop and in the main scroll position on mobile */}
+    const selectedPanelContainerStyle = {display:"flex", flexDirection:"column", borderColor:"#49A3DC", borderWidth:"2px", boxShadow:"0 0 2px #49A3DC", marginBottom:"0px"};
     const panelBordersColor = this.props.selected ? "#49A3DC" : "#CCCCCC";
+
+    {/* Below code is temporary, just for simulating different RS values */}
+    var retailScore = <label className="control-label" style={{fontSize:"12px", marginLeft:"10px", color:"#27ae60", borderBottom:"solid thin"}}>Retail Score: <i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/></label>;
+    if(this.props.index == 1) {
+      retailScore = <label className="control-label" style={{fontSize:"12px", marginLeft:"10px", color:"#f1c40f", borderBottom:"solid thin"}}>Retail Score: <i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/></label>;
+    } else if(this.props.index == 2) {
+      retailScore = <label className="control-label" style={{fontSize:"12px", marginLeft:"10px", color:"#e67e22", borderBottom:"solid thin"}}>Retail Score: <i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/></label>;
+    } else if(this.props.index == 3) {
+      retailScore = <label className="control-label" style={{fontSize:"12px", marginLeft:"10px", color:"#c0392b", borderBottom:"solid thin"}}>Retail Score: <i className="fa fa-star"/><i className="fa fa-star"/></label>;
+    } else if(this.props.index == 4) {
+      retailScore = <label className="control-label" style={{fontSize:"12px", marginLeft:"10px", color:"#c0392b", borderBottom:"solid thin"}}>Retail Score: <i className="fa fa-star"/></label>;
+    }
+
     return(
-      <div className="panelContainer" onMouseOver={() => this.props.onHover(this.props.index)}>
-        <div className="panel b text-center srpTilePanel" style={this.props.selected ? selectedPanelContainerStyle : {display:"flex", flexDirection:"column", borderColor:"#CCCCCC", borderWidth:"2px", marginBottom:"0px"}}>
+      <div className="panelContainer" onMouseOver={() => this.props.mobile ? false : this.props.onHover(this.props.index)}>
+      {/* For desktop we want hover to put this panel in focus, so check flag and call prop function if needed */}
+        {/* Panels are of a fixed height, using media queries to adjust based on device width */}
+      {/* If a panel is selected we want the blue action color, otherwise the gray normal color */}
+        <div className="panel b text-center srpTilePanel" style={this.props.selected ? selectedPanelContainerStyle : {borderColor:"#CCCCCC", borderWidth:"2px", marginBottom:"0px"}}>
           <div className="panel-body" style={{padding:"0px"}}>
+            {/* Image is of a specific height, this changes in CSS depending on screen width */}
             <div className="srpTilePanelImageHeight" style={{backgroundColor:"rgba(255,0,0,0.1)"}} />
           </div>
-          <div className="panel-body bt" style={{padding:"2px", borderColor: panelBordersColor, flexGrow:"1", maxHeight:"100px"}}>
-            <div style={{display:"flex", justifyContent:"space-around", alignItems:"center"}}>
-              <label className="control-label" style={{fontSize:"12px", marginLeft:"10px", color:"#27ae60", borderBottom:"solid thin"}}>Retail Score: <i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/><i className="fa fa-star"/></label>
-            </div>
+          {/* RS and explanation, explanation uses CSS text cutoof technique on smaller devices with varying # of lines depending on screen width */}
+          <div className="panel-body bt" style={{padding:"2px", borderColor: panelBordersColor}}>
+            {retailScore}
             <p className="lineClamp">6 other businesses in this area attract high-end shoppers looking for casual clothing, by locating here your business will be another option for these customers.</p>
           </div>
+          {/* Price and SQ FT info shown side by side, margins are different to account for padding of other elements */}
           <div className="panel-body bt" style={{padding:"2px", borderColor: panelBordersColor}}>
             <Row>
               <Col xs={6} className="br" style={{borderColor: panelBordersColor}}>
@@ -145,8 +188,10 @@ class PropertyTile extends React.Component {
               </Col>
             </Row>
           </div>
+          {/* Button that in 1-Tap contact mode sends a message to Broker, otherwise takes user to PDP -> this avoid more media query logic to take out the button and resize entire tile */}
+          {/* Button is not visible on phones <= 320px wide (iPhone 4 and 5) because it will not fit, CSS media query catches this */}
           <div className="panel-body bt srpContactButton" style={{borderColor: panelBordersColor, margin:"0px", padding:"0px"}}>
-            <Button style={{border:"none", color:"#49A3DC", width:"100%"}}>Contact</Button>
+            <Button style={{border:"none", color:"#49A3DC", width:"100%", paddingBottom:"5px"}}>Contact</Button>
           </div>
         </div>
       </div>
@@ -155,6 +200,7 @@ class PropertyTile extends React.Component {
 }
 class SearchBar extends React.Component {
   render() {
+  {/* styles to use when on mobile to give sharp edges on top bar, round edges stick out */}
     const mobileMenuButtonStyle = {height:"50px", borderColor:"#CCCCCC", borderRadius: "0", borderTop:"none", borderLeft:"none", borderBottom:"none"};
     const mobileSearchBarStyle = {height:"50px", borderColor:"#CCCCCC", borderRadius: "0", borderTop:"none", borderLeft:"none", borderBottom:"none"};
     const mobileSearchButtonStyle = {height:"50px", borderColor:"#CCCCCC", borderRadius: "0", borderTop:"none", borderRight:"none", borderBottom:"none"};
