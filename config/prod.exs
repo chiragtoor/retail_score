5dad5b19a33ae7194e6096143fdcc823cf2385a6
Chirag Singh Toor <chirag@Chirags-MacBook-Pro.local>
2016-10-18 21:05:49 -0700

    Change Prod DB url to new upgraded DB

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :logger, level: :info
 # Configure your database
 config :retail_score, RetailScore.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: System.get_env("HEROKU_POSTGRESQL_ONYX_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
